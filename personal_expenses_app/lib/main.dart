ea3e5f6558a09d422505c82736196c9f52f161b9
FlorianFeka <florianfeka05@gmail.com>
2020-10-05 17:48:36 +0200

    77. Styling Text

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -61,16 +61,33 @@ class MyHomePage extends StatelessWidget {
                             EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                         decoration: BoxDecoration(
                             border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         )),
                         padding: EdgeInsets.all(10),
-                        child: Text(tx.amount.toString()),
+                        child: Text(
+                          tx.amount.toString(),
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(tx.titel),
-                          Text(tx.date.toString()),
+                          Text(
+                            tx.titel,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            tx.date.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          ),
                         ],
                       )
                     ],
