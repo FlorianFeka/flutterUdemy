1ad0743ca48de58dab836cc6beaf786014dad84d
FlorianFeka <florianfeka05@gmail.com>
2020-10-05 22:40:28 +0200

    91. Adding AppBar Buttons & Floating Action Buttons

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -28,6 +28,12 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -43,6 +49,11 @@ class MyHomePage extends StatelessWidget {
               ),
               UserTransactions()
             ]),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
