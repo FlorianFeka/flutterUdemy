28ce64c207c3ff1df87d436b7dad90770637e1d8
FlorianFeka <florianfeka05@gmail.com>
2020-10-11 15:16:30 +0200

    106. Improving the Overall Chart

--- personal_expenses_app/lib/widgets/chart_bar.dart
+++ personal_expenses_app/lib/widgets/chart_bar.dart
@@ -11,8 +11,15 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(child: Text('\$${spendingAmount.toStringAsFixed(0)}')),
-        SizedBox(height: 4),
+        Container(
+          height: 20,
+          child: FittedBox(
+            child: Text('\$${spendingAmount.toStringAsFixed(0)}'),
+          ),
+        ),
+        SizedBox(
+          height: 4,
+        ),
         Container(
           height: 60,
           width: 10,
@@ -20,7 +27,10 @@ class ChartBar extends StatelessWidget {
             children: <Widget>[
               Container(
                 decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1.0),
+                  border: Border.all(
+                    color: Colors.grey,
+                    width: 1.0,
+                  ),
                   color: Color.fromRGBO(220, 220, 220, 1),
                   borderRadius: BorderRadius.circular(10),
                 ),
