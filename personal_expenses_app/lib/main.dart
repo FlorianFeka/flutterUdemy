f75c618ddd5a15b749da076b0741206fef4682c6
FlorianFeka <florianfeka05@gmail.com>
2020-10-15 00:53:43 +0200

    131. More Cupertino Styles

--- personal_expenses_app/lib/main.dart
+++ personal_expenses_app/lib/main.dart
@@ -156,7 +156,10 @@ class _MyHomePageState extends State<MyHomePage> {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text('Show Chart'),
+                    Text(
+                      'Show Chart',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
                     Switch.adaptive(
                       value: _showChart,
                       activeColor: Theme.of(context).accentColor,
